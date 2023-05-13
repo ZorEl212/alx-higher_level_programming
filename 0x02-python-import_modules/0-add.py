c475818b1393adf2c94288785e052c784273baf9
ZorEl212 <yabsirad212@gmail.com>
2023-05-13 12:28:30 -0400

    Change print format

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,7 +2,7 @@
 
 if (__name__ == '__main__'):
     from add_0 import add
+
     a = 1
     b = 2
-    result = add(a, b)
-    print(f"{a} + {b} = {result}")
+    print("{0} + {1} = {2}".format(a, b, add(a, b)))
