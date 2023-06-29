d4b3c84cd4a291248df637ed35795b4d6c37e478
ZorEl212 <yabsirad212@gmail.com>
2023-06-29 17:05:37 -0400

    Fix up the case for x = 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     def safe_print_list(my_list=[], x=0):
+        i = 0
         for i in range(0, x):
             try:
                 print("{}".format(my_list[i]), end="")
+                i= i+1
             except IndexError:
                 print()
                 return i
         print()
-        return i+1
+        return i
         
-
