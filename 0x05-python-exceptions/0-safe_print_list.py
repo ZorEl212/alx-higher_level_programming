c44dd7075689576a7b12e4acad14df557b85f07f
ZorEl212 <yabsirad212@gmail.com>
2023-06-29 17:22:43 -0400

    Fix importing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def safe_print_list(my_list=[], x=0):
-        i = 0
-        for i in range(0, x):
-            try:
-                print("{}".format(my_list[i]), end="")
-                i= i+1
-            except IndexError:
-                print()
-                return i
-        print()
-        return i
-        
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    for i in range(0, x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            i= i+1
+        except IndexError:
+            print()
+            return i
+    print()
+    return i
+if __name__ == '__main__':
+    safe_print_list()
+    
