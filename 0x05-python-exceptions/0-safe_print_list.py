b37a210fb839a354ee9d80340a25bfbb70e101dc
ZorEl212 <yabsirad212@gmail.com>
2023-06-29 17:27:09 -0400

    fix pycodestyle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,12 +4,13 @@ def safe_print_list(my_list=[], x=0):
     for i in range(0, x):
         try:
             print("{}".format(my_list[i]), end="")
-            i= i+1
+            i = i + 1
         except IndexError:
             print()
             return i
     print()
     return i
+
+
 if __name__ == '__main__':
     safe_print_list()
-    
