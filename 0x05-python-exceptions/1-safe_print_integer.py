cb1b25ff32f804803143b6884387d3f1d15f3784
ZorEl212 <yabsirad212@gmail.com>
2023-07-01 12:59:03 -0400

    print a intiger

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+    except ValueError:
+        return False
+    else: 
+        return True
+
+
+if __name__ == "__main__":
+    safe_print_integer()
