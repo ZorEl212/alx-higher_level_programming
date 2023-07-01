eb2276bef7ac61b9907f76a2b7ed584b58d4dc47
ZorEl212 <yabsirad212@gmail.com>
2023-07-01 13:48:00 -0400

    Fix pycodestyle

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0  # Variable to keep track of the number of integers printed
-    
+    count = 0
+
     for i in range(x):
         try:
-            if isinstance(my_list[i], int):  # Check if the element is an integer
+            if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
         except IndexError:
-            break  # Stop the loop if we reach the end of the list
-    
-    print()  # Print a new line after all integers are printed
+            break
+
+    print()
     return count
