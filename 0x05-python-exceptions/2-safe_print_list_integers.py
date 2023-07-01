269282fcac2266b31278c096d09e2da1f9292d80
ZorEl212 <yabsirad212@gmail.com>
2023-07-01 13:32:59 -0400

    Print integers only

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0  # Variable to keep track of the number of integers printed
+    
+    for i in range(x):
+        try:
+            if isinstance(my_list[i], int):  # Check if the element is an integer
+                print("{:d}".format(my_list[i]), end="")
+                count += 1
+        except IndexError:
+            break  # Stop the loop if we reach the end of the list
+    
+    print()  # Print a new line after all integers are printed
+    return count
