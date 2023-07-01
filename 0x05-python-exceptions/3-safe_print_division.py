3ac86e8c22a953ca4dde73916890e4b447d3a90b
ZorEl212 <yabsirad212@gmail.com>
2023-07-01 14:56:42 -0400

    Divide integers

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    div = None
+    try:
+        div = a / b
+    except ZeroDivisionError:
+        return None
+    finally:
+        print("{}".format(div))
+    return div
