ebefa367b9f64c9362e60c8f22ff547503c9c172
ZorEl212 <yabsirad212@gmail.com>
2023-07-01 14:59:05 -0400

    Fix print message

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,5 +7,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         return None
     finally:
-        print("{}".format(div))
+        print("Inside result:{}".format(div))
     return div
