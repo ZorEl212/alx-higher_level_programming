268c70b604a093c8f30db99402bcbd5850ebf5de
ZorEl212 <yabsirad212@gmail.com>
2023-07-03 12:01:00 -0400

    Initialize the class with a size (required field) Set the size to private

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+class Square:
+    """Simple square class"""
+    def __init__(self, size):
+        """init
+        args:
+            size: size of the square"""
+        self.__size = size
