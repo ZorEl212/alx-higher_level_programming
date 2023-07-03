6e8af284b508ad48fad42d5025d02e9b1baee749
ZorEl212 <yabsirad212@gmail.com>
2023-07-03 12:02:08 -0400

    Raise exceptions in case of inappropriate input. make the size optional input

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+class Square:
+    """Simple square class"""
+
+    def __init__(self, size=0):
+        """init
+        args:
+            size: size of the square"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif int(size) < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
