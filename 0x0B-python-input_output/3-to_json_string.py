8f3d847684b7f63751312df749b21b01ae2cee43
ZorEl212 <yabsirad212@gmail.com>
2023-07-11 14:39:44 -0400

    Serialize an object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!usr/bin/python3
+
+"""Module containing obj-jason function"""
+
+
+from json import dumps
+
+
+def to_json_string(my_obj):
+    """Return json format of an object
+        args:
+            my_obj: Object to serialize"""
+
+    return dumps(my_obj)
