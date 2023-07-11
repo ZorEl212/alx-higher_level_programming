ba4151ab84b7277664859e6f76f43d53273a9cb8
ZorEl212 <yabsirad212@gmail.com>
2023-07-11 13:44:36 -0400

    Fix new line

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,4 +9,4 @@ def read_file(filename=""):
             filename: name of the file"""
 
     with open(filename, encoding='utf-8') as myfile:
-        print(myfile.read())
+        print(myfile.read(), end="")
