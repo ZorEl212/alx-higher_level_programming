0dac9b882194c3c638587eb831e7671b4eabf0dc
ZorEl212 <yabsirad212@gmail.com>
2023-07-12 13:47:28 -0400

    im sys

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,15 +2,15 @@
 
 """Module to save provided args to json"""
 
-from sys import argv
+import sys
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
          __import__('6-load_from_json_file').load_from_json_file 
     try:
-        args_list = load_from_json_file("add_items.json")
+        items = load_from_json_file("add_items.json")
     except FileNotFoundError:
-        args_list = []
-        args_list.extend(argv[1:])
-    save_to_json_file(args_list, "add_item.json")
+        items = []
+        items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
