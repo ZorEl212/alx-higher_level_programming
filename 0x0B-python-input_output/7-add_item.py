c4166f17d2deb8648b44160d4ec920414c4c69e0
ZorEl212 <yabsirad212@gmail.com>
2023-07-12 13:04:40 -0400

    Test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,8 +3,13 @@
 """Module to save provided args to json"""
 
 from sys import argv
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-
-args_list = list(argv[1:])
-save_to_json_file(args_list, "add_item.json")
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+         __import__('6-load_from_json_file').load_from_json_file 
+    try:
+        args_list = load_from_json_file("add_items.json")
+    except FileNotFoundError:
+        args_list = list(argv[1:])
+    save_to_json_file(args_list, "add_item.json")
