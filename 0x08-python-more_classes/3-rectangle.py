e6c1164f1a5cae92c4187c8dd24722ae73dcd2ff
ZorEl212 <yabsirad212@gmail.com>
2023-09-16 15:37:45 -0400

    2-rectangle + __str__ implementation

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Module for 1-rectangle"""
+
+
+class Rectangle:
+    """Rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """getattr for width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set value for width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if (value < 0):
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """getattr for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set value for height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if (value < 0):
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return area of the rectangle"""
+        return (self.height * self.width)
+
+    def perimeter(self):
+        """Return perimeter of the rectangle"""
+        return 2 * (self.height + self.width)
+
+    def __str__(self):
+        s = ""
+        for i in range(self.height):
+            s += ('#' * self.width) + '\n'
+        return s
