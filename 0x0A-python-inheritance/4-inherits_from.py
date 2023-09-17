a46786454bddc33107f410b3cadc13eb3b862a0c
ZorEl212 <yabsirad212@gmail.com>
2023-09-17 07:08:55 -0400

    Check if obj is inherits from superclasses

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module for class checking"""
+
+
+def inherits_from(obj, a_class):
+    """Check if an object blongs to a class
+    or inherites from other class"""
+    if (isinstance(obj, a_class)):
+        if (type(obj) == a_class):
+            return False
+        return True
