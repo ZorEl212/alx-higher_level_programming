f54dc7f81d27acb34372979bbf881a027ee66545
ZorEl212 <yabsirad212@gmail.com>
2023-09-17 06:49:43 -0400

    Refactor: check for exact class match Instead of using 'isinstance' which checks for inherited classes. use type for exact match

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,7 +4,4 @@
 
 def is_same_class(obj, a_class):
     """Check if an obj is instance of a class"""
-    if (isinstance(obj, a_class)):
-        return True
-    else:
-        return False
+    return type(obj) == a_class
