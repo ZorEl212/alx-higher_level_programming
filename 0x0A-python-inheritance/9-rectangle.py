22fcb019950b0733f7f5cffb998d1ab12f22ae09
ZorEl212 <yabsirad212@gmail.com>
2023-09-17 08:44:14 -0400

    Rectangle with area implemented and magic method __str__

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module for Rectangle class inherited from Geometry"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class inheriting from BaseGeometry"""
+    def __init__(self, width, height):
+        BaseGeometry.integer_validator(self, "width", width)
+        BaseGeometry.integer_validator(self, "height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__height * self.__width
+
+    def __str__(self):
+        return f'[Rectangele] {self.__width}/{self.__height}'
