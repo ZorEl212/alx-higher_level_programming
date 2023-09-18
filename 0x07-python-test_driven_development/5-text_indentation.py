fa909a13cbf94b44c17dc8aa41b718c2ea6ac9e9
ZorEl212 <yabsirad212@gmail.com>
2023-09-18 13:09:58 -0400

    Print text with some rules + Doctest

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module for text_indentation function"""
+
+
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    ptext = []
+    puncutations = [".", "?", ":"]
+    for char in range(len(text)):
+        if text[char] == " " and (len(ptext) == 0 or ptext[-1] in puncutations \
+            or text[char-1] == "\n"):
+            continue
+        if text[char] in puncutations:
+            print(text[char])
+            ptext.append(text[char])
+            print()
+            continue
+        print(text[char], end="")
+        ptext.append(text[char])
+        
