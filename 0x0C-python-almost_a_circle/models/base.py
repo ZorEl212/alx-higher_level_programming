aaa678fd80a063845aac75affa07472a552b1407
ZorEl212 <yabsirad212@gmail.com>
2023-09-30 17:27:28 -0400

    Method to return json representation of list dicts

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+import json
 """Module for base class"""
 
 
@@ -23,3 +23,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON serialization of a list of dicts.
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
