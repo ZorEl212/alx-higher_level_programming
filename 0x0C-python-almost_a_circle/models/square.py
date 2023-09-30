f16bf1caa225c50ff131ff620b30a5b3a84b3034
ZorEl212 <yabsirad212@gmail.com>
2023-09-30 17:16:10 -0400

    Update method for square a little modified than the rectangle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -23,6 +23,21 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
+    def update(self, *args, **kwargs):
+        """Update the Square."""
+
+        arg_names = ["id", "size", "x", "y"]
+
+        if args:
+            for i, arg in enumerate(args):
+                if i < len(arg_names) and arg is not None:
+                    setattr(self, arg_names[i], arg)
+            return
+
+        for k, v in kwargs.items():
+            if k in arg_names and v is not None:
+                setattr(self, k, v)
+
     def __str__(self):
         return (f'[Square] ({self.id}) {self.x}/{self.y} - '
                 f'{self.size}')
