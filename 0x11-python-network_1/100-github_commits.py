1539306c825cbae53531daa84530691965412bcf
ZorEl212 <yabsirad212@gmail.com>
2023-12-01 23:17:19 -0500

    Fix Doc and Don't execute when imported

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
+"""Get the last 10 commit sha and Author from a repo"""
 import requests
 import sys
 
 
-repo = sys.argv[1]
-owner = sys.argv[2]
+if __name__ == "__main__":
+    repo = sys.argv[1]
+    owner = sys.argv[2]
 
-url = f"https://api.github.com/repos/{owner}/{repo}/commits"
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
 
-res = requests.get(url)
-for i in range(10):
-    print(f"{res.json()[i]['sha']}: {res.json()[i]['commit']['author']['name']}")
+    res = requests.get(url)
+    for i in range(10):
+        print(f"{res.json()[i]['sha']}: {res.json()[i]['commit']['author']['name']}")
