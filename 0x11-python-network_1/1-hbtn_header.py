5ee3cbad34b7a09b15e3594a759a0efa0985813d
ZorEl212 <yabsirad212@gmail.com>
2023-12-01 20:41:23 -0500

    Don't run the scripts when imported

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-# A python script to get header of a http request
+""" A python script to get header of a http request"""
 
 import urllib.request
 import sys
 
-
-url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
-    html = response.read()
-    print(response.headers.get('X-Request-Id'))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print(response.headers.get('X-Request-Id'))
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,14 +5,14 @@ import urllib.request
 import urllib.parse
 import sys
 
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-url = sys.argv[1]
-email = sys.argv[2]
-
-values = {'email': email}
-data = urllib.parse.urlencode(values)
-data = data.encode('ascii')
-req = urllib.request.Request(url, data)
-with urllib.request.urlopen(req) as response:
-    html = response.read()
-    print(html.decode('utf-8'))
+    values = {'email': email}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print(html.decode('utf-8'))
