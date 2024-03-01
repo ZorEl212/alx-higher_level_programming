e84e315f84d0444cce6395f02d5d05fa4f0285bb
ZorEl212 <yabsirad212@gmail.com>
2024-03-01 20:40:24 +0300

    Fix semistandard errors

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 /* Check passed arguments to the script */
-const args = process.argv
-if (args.length > 3){
-    console.log('Arguments found');
-}
-else if (args.length == 3){
-    console.log('Argument found');
-}
-else{
-    console.log('No argument');
+const args = process.argv;
+if (args.length > 3) {
+  console.log('Arguments found');
+} else if (args.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('No argument');
 }
