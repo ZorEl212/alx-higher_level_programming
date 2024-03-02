c30e9e8859c75ea4fcff3f449444b9331bf6cf3d
ZorEl212 <yabsirad212@proton.me>
2024-03-02 16:11:19 +0300

    Fix semistandard
    Signed-off-by: ZorEl212 <yabsirad212@proton.me>

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,6 +2,6 @@
 /* print c is fun based on passed argument */
 
 const cIsFun = 'C is fun';
-for (i = 0; i < Number(process.argv[2]); i++) {
-    console.log(cIsFun);
+for (let i = 0; i < Number(process.argv[2]); i++) {
+  console.log(cIsFun);
 }
